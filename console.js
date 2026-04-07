bf11a4f99fd5846594471551ced2306bbd51c85a
akshit3022 <akshit2953z+free@gmail.com>
2026-04-07 12:37:36 +0000

    installation of MongoDB Tools

---
+++ console.js
@@ -0,0 +1,10 @@
+// wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2204-x86_64-100.9.4.tgz
+
+// tar -xvzf mongodb-database-tools-*.tgz
+
+// cd mongodb-database-tools-*/bin
+
+// sudo cp * /usr/local/bin/
+
+// mongodump --version
+// mongorestore --version
